419197511a56299b6a4350cd5fe23c641efdc65a
astralicc <87800630+astralicc@users.noreply.github.com>
2025-07-10 12:29:09 +0700

    Add files via upload

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+import { Image, ScrollView, Text, View } from "react-native";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import { SearchBar } from "react-native-screens";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+        resizeMode="cover"
+      />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.squirtle} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        
+        <View className="flex-1 mt-5">
+          <SearchBar/>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
---
+++ components/SearchBar.tsx
@@ -0,0 +1,20 @@
+import { View, Text, TextInput, Image } from 'react-native'
+import { icons } from '@/constants/icons'
+
+const SearchBar = () => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#AB8BFF"/>
+      <TextInput
+        onPress={() => { }}
+        placeholder="Search"
+        value=""
+        onChangeText={() => { }}
+        placeholderTextColor="#A8B5DB"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  )
+}
+
+export default SearchBar
